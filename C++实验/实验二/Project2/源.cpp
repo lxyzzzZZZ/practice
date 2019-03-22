cae6e17c3b29b7646350f37518b2e2d0a6150358
lxyzzzZZZ <1909980451@qq.com>
2019-03-22 22:07:28 +0800

    提交代码

---
+++ C++实验/实验二/Project2/源.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//using namespace std;
+//typedef struct Student{
+//	char arr[20];
+//	int* Grade;
+//	int sum;
+//}*stu;
+//void Init(stu s,int N){
+//	cout << "请输入学生姓名：";
+//	cin >> s->arr;
+//	for (int i = 0; i < N; i++){
+//		cout << "请输入成绩";
+//		cin >> s->Grade[i];
+//		s->sum += s->Grade[i];
+//	}
+//}
+//void deplay(stu s){
+//	cout << "学生：" << s->arr << "成绩：" << s->Grade << "总成绩：" << s->sum << endl;
+//}
+//void  Menu(){
+//	cout << "------  请选择  ------" << endl;
+//	cout << "------1.添加信息------" << endl;
+//	cout << "------2.查看信息------" << endl;
+//	cout << "------3.  退出  ------" << endl;
+//}
+//int main(){
+//	while (1){
+//		switch ()
+//	}
+//	system("pause");
+//}
