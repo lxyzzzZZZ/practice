25d678179043bd0431f162bbc229e0c1166d0223
lxyzzzZZZ <1909980451@qq.com>
2019-03-27 21:09:49 +0800

    提交代码

---
+++ C++实验/实验2/源.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void BubbleSort(int* arr, int size){
+	for (int i = 0; i < size; i++){
+		int tmp = arr[i];
+		for (int j = i + 1; j < size; j++){
+			if (arr[i] > arr[j]){
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+	return;
+}
+void deplay(int* arr, int size){
+	for (int i = 0; i < size; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+int main(){
+	int arr[10] = { 1, 2, 4, 5, 6, 4, 3, 1, 4, 9 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, size);
+	deplay(arr, size);
+	system("pause");
+}
